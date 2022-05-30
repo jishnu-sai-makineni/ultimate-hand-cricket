0afe19540c0597491e4759ad21acf693119e888e
Varun Karthik <varunkarthikcoc@gmail.com>
2022-05-30 21:57:00 +0530

    sadf

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,7 +9,7 @@ export default function App(){
         <Col className="w-50">
           <Card>
             <Card.Img style={{width: "100%", height: "300px"}} src="https://www.colorhexa.com/42ffff.png" />
-            <Link className='stretched-link' href='/?a=nyom'></Link>
+            <Link className='stretched-link' href='/?a=nyom'><a href='/?a=nyom'></a></Link>
             <Card.ImgOverlay>
               <Card.Title>Matchmaking with randy</Card.Title>
             </Card.ImgOverlay>
@@ -18,7 +18,7 @@ export default function App(){
         <Col className="w-50">
           <Card >
             <Card.Img style={{width: "100%", height: "300px"}} src="https://www.colorhexa.com/69facd.png" />
-              <Link className='stretched-link' href='/joinGame'></Link>
+              <Link className='stretched-link' href='/joinGame'><a href='/joinGame'></a></Link>
               <Card.ImgOverlay>
                 <Card.Title>Join a Game</Card.Title>
               </Card.ImgOverlay>
@@ -29,7 +29,7 @@ export default function App(){
         <Col className="w-50">
           <Card >
             <Card.Img style={{width: "100%", height: "300px"}} src="https://www.colorhexa.com/27caeb.png" />
-            <Link className='stretched-link' href='/createGame'></Link>
+            <Link className='stretched-link' href='/createGame'><a href='/createGame'></a></Link>
             <Card.ImgOverlay>
                 <Card.Title>Create a Game</Card.Title>
             </Card.ImgOverlay>
@@ -38,7 +38,7 @@ export default function App(){
         <Col className="w-50">
           <Card >
             <Card.Img style={{width: "100%", height: "300px"}} src="https://www.colorhexa.com/82feeb.png" />
-            <Link className='stretched-link' href='/?a=nyoooom'></Link>
+            <Link className='stretched-link' href='/?a=nyoooom'><a href='/?a=nyoooom'></a></Link>
             <Card.ImgOverlay>
               <Card.Title>TO BE DECLARED</Card.Title>
             </Card.ImgOverlay>
